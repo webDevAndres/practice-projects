69cb8fc22059298c791a57a908c71bf5050d4e47
webdevandres <maciasandres02@gmail.com>
2021-01-21 11:54:32 -0600

    added data validation

--- mpg.js
+++ mpg.js
@@ -12,35 +12,76 @@ var calculateMpg = function (miles, gallons) {
 };
 
 var createNodes = function (mpg) {
-        //create a new node
-        var inputNode = document.createElement('input');
-        var labelNode = document.createElement('label');
-        //set up input element with type, id and the disabled attribute
-        inputNode.type = "text";
-        inputNode.id = "mpg_result";
-        inputNode.setAttribute("disabled", true);
-        //get the calculated mpg from the parameter and set the value of the inputNode
-        inputNode.value = mpg;
-        //set the the text and attribute of the label element
-        labelNode.textContent = "Miles Per Gallon";
-        labelNode.setAttribute("for", "mpg_result");
+    //create a new node
+    var inputNode = document.createElement('input');
+    var labelNode = document.createElement('label');
+    //set up input element with type, id and the disabled attribute
+    inputNode.type = "text";
+    inputNode.id = "mpg_result";
+    inputNode.setAttribute("disabled", true);
+    //get the calculated mpg from the parameter and set the value of the inputNode
+    inputNode.value = mpg;
+    //set the the text and attribute of the label element
+    labelNode.textContent = "Miles Per Gallon";
+    labelNode.setAttribute("for", "mpg_result");
+
+    if (!isNaN(inputNode.value)) {
         //attach the nodes to the end of the form
         var parentDiv = document.getElementById("main_content");
         labelNode.appendChild(inputNode);
         parentDiv.appendChild(labelNode);
+    }
 
 };
 
-var displayResult = function () {
-    var userMiles = parseFloat(document.getElementById("miles").value);
-    var userGallons = parseFloat(document.getElementById("gallons").value);
 
-    if (isNaN(userMiles) || isNaN(userGallons)) {
-        alert("Both entries must be numeric.");
+var displayResult = function () {
+    var userMiles = parseFloat($("miles").value);
+    var userGallons = parseFloat($("gallons").value);
+    var result = validateData(userMiles, userGallons);
+    var resultElement = $("mpg_result");
+
+    if (resultElement) {
+        var currentValue = resultElement.value;
+        var newValue = result;
+
+        if (currentValue !== newValue) {
+            resultElement.value = newValue;
+        }
     } else {
-        var mpg = calculateMpg(userMiles, userGallons);
-        createNodes(mpg);
+        createNodes(result);
+
     }
+};
+
+var validateData = function (userMiles, userGallons) {
+    var isValid = true;
+
+    if (isNaN(userMiles)) {
+        $("miles_error").firstChild.nodeValue = "Miles must be numeric.";
+        isValid = false;
+    } else if (userMiles < 0) {
+        $("miles_error").firstChild.nodeValue = "Miles must be greater than zero";
+        isValid = false;
+    } else {
+        $("miles_error").firstChild.nodeValue = "";
+    }
+
+    if (isNaN(userGallons)) {
+        $("gallons_error").firstChild.nodeValue = "Gallons must be numeric.";
+        isValid = false;
+    } else if (userGallons < 0) {
+        $("gallons_error").firstChild.nodeValue = "Gallons must be greater than 0";
+        isValid = false;
+    } else {
+        $("gallons_error").firstChild.nodeValue = "";
+    }
+
+    if (isValid) {
+        var mpg = calculateMpg(userMiles, userGallons);
+        return mpg;
+    }
+
 };
 
 
